5edbfdd8fc1cb779358d28bf0e2c97e0e31d8251
Bryan Davis <bd808@wikimedia.org>
2020-09-09 16:13:12 -0600

    Fix copy-n-paste mentions of Striker
    Fix GPL preamble blocks which mention the "Striker" project to instead
    refer to this project. This was a copy-n-paste error made originally in
    settings.py and then propagated to other files.

--- toolhub/settings.py
+++ toolhub/settings.py
@@ -14,7 +14,7 @@
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with Striker.  If not, see <http://www.gnu.org/licenses/>.
+# along with Toolhub.  If not, see <http://www.gnu.org/licenses/>.
 """
 Django settings for toolhub project.
 
