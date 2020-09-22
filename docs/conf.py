c13470c453126a27bb0254e43354c3ec86af2ee0
Bryan Davis <bd808@wikimedia.org>
2020-09-22 09:43:03 -0600

    sphinx: fix Black warning

--- docs/conf.py
+++ docs/conf.py
@@ -63,7 +63,7 @@ html_theme = "sphinx_rtd_theme"
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 html_static_path = ["_static"]
 html_css_files = [
-    'css/custom.css',
+    "css/custom.css",
 ]
 htmlhelp_basename = "Toolhubdoc"
 
