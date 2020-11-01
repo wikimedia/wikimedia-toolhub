0e23ce3340c59253f672fe3e7710a2fb127cb8a1
Bryan Davis <bd808@wikimedia.org>
2020-11-01 13:47:48 -0700

    ui: prettier home screen
    Use a card as a jumbotron style element on the home screen.
    
    Change-Id: Iacdacb1ff0a442495ac8d2dfaf0b2abe774bdffc

--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -1,14 +1,32 @@
 <template>
-	<v-container>
-		<v-row>
-			<v-col class="mb-4">
-				<h1 class="display-2 font-weight-bold mb-3">
-					Welcome to Toolhub
-				</h1>
-				<p class="subheading">
-					An authoritative and well promoted catalog of Wikimedia tools.
-				</p>
-			</v-col>
-		</v-row>
-	</v-container>
+	<v-row dense>
+		<v-col cols="12">
+			<v-card
+				class="mx-auto"
+				outlined
+			>
+				<div class="d-flex flex-no-wrap justify-space-between">
+					<div>
+						<v-card-title class="headline">
+							Welcome to Toolhub
+						</v-card-title>
+						<v-card-subtitle>
+							An authoritative and well promoted catalog of Wikimedia tools.
+						</v-card-subtitle>
+						<v-card-text>
+							More text would go here and tell people something
+							awesome about Toolhub or how to get started using it.
+						</v-card-text>
+					</div>
+					<v-avatar
+						class="ma-3"
+						size="125"
+						tile
+					>
+						<v-img src="/static/img/logo-community.svg" />
+					</v-avatar>
+				</div>
+			</v-card>
+		</v-col>
+	</v-row>
 </template>
