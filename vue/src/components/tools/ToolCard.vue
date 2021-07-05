9f4a353078f351f2ee81fc417b32bdff2738ecc8
srishakatux <ssethi@wikimedia.org>
2021-07-04 20:28:26 -0700

    Improve home page and tool card design
    Changes include:
    * Removed keywords
    * Made the entire card clickable
    * Removed the two big blue buttons from tool card
    * Reduced the tool card's height
    * Changed the word "authoritative" to "community-authored"
    * Font / icon size changes in the header
    * Move Toolhub logo on the left
    * Drop the border of box header
    * Move search below the box header
    
    Bug: T286164
    Change-Id: I0ae8d39e3dccfeff527d155d7405b9d83c2c5f62

--- vue/src/components/tools/ToolCard.vue
+++ vue/src/components/tools/ToolCard.vue
@@ -1,7 +1,8 @@
 <template>
 	<v-card
-		class="tool-card__card"
-		height="550px"
+		class="tool-card__card pa-1"
+		height="220px"
+		:to="`/tool/${tool.name}`"
 	>
 		<v-card-title class="flex-nowrap">
 			<CommonsImage
@@ -17,57 +18,11 @@
 			<div class="tool-card__desc text--primary line-clamp" dir="auto">
 				{{ tool.description }}
 			</div>
-			<dl class="row mx-0 my-4 text-subtitle-1">
+			<dl class="tool-card__authors row mx-0 my-4 text-subtitle-1">
 				<dt class="me-1">{{ $t( 'authors' ) }}:</dt>
-				<dd>{{ tool.author }}</dd>
+				<dd class="line-clamp">{{ tool.author }}</dd>
 			</dl>
-			<v-divider />
 		</v-card-text>
-		<v-card-text class="py-0">
-			<div v-if="tool.keywords">
-				<v-chip
-					v-for="tt in tool.keywords.filter(e => e).slice(0, 5)"
-					:key="tt"
-					:ripple="false"
-					disabled
-					class="ma-1 opacity-1"
-				>
-					{{ tt }}
-				</v-chip>
-			</div>
-		</v-card-text>
-		<v-card-actions class="ma-2">
-			<v-btn
-				color="primary"
-				dark
-				block
-				:to="`/tool/${tool.name}`"
-			>
-				<v-icon
-					dark
-					class="pe-1"
-				>
-					mdi-information-outline
-				</v-icon>
-				{{ $t( 'learnmore' ) }}
-			</v-btn>
-			<v-spacer />
-			<v-btn
-				color="primary"
-				dark
-				block
-				:href="`${tool.url}`"
-				target="_blank"
-			>
-				<v-icon
-					dark
-					class="pe-1"
-				>
-					mdi-link-variant
-				</v-icon>
-				{{ $t( 'browsetool' ) }}
-			</v-btn>
-		</v-card-actions>
 	</v-card>
 </template>
 
--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -1,92 +1,65 @@
 <template>
 	<v-container>
 		<v-row>
-			<v-col cols="12">
-				<SearchBar
-					@search="onSearchBarSearch"
-				/>
+			<v-col cols="12" class="pa-0">
+				<v-row no-gutters>
+					<v-col sm="auto" cols="12">
+						<v-avatar
+							class="ma-3"
+							size="150"
+							tile
+						>
+							<v-img src="/static/img/logo-community.svg" />
+						</v-avatar>
+					</v-col>
+
+					<v-col>
+						<h1 class="text-h4 ma-1">
+							{{ $t( 'welcomemessage' ) }}
+						</h1>
+						<div class="text-subtitle-1 mx-1">
+							{{ $t( 'tagline-about' ) }}
+						</div>
+
+						<dl v-if="lastCrawlerRun" class="row ma-1">
+							<dt class="me-1">
+								<v-icon size="20">
+									mdi-tools
+								</v-icon>
+								{{ $t( 'toolsfound', [ numTools ] ) }}
+							</dt>
+							<dd>
+								<v-icon
+									size="21"
+								>
+									mdi-update
+								</v-icon>
+								{{ $t(
+									'newtoolsfound',
+									[
+										lastCrawlerRun.new_tools +
+											lastCrawlerRun.updated_tools
+									]
+								) }}
+								{{ $t(
+									'tools-lastupdated',
+									[
+										formatDate(
+											lastCrawlerRun.end_date
+										)
+									]
+								) }}
+							</dd>
+						</dl>
+					</v-col>
+				</v-row>
 			</v-col>
 		</v-row>
 		<v-row>
 			<v-col cols="12">
-				<v-card
-					class="mx-auto"
-					outlined
-					elevation="1"
-				>
-					<v-row no-gutters>
-						<v-col>
-							<h2 class="text-h4 ma-4">
-								{{ $t( 'welcomemessage' ) }}
-							</h2>
-							<div class="me-4 ms-4">
-								{{ $t( 'tagline-about' ) }}
-							</div>
-							<div class="me-4 ms-4">
-								<a href="https://meta.wikimedia.org/wiki/Toolhub"
-									target="_blank"
-								>
-									{{ $t( 'tagline-learnmore' ) }}
-								</a>
-							</div>
-
-							<v-row
-								v-if="lastCrawlerRun"
-								class="mx-1 my-3 text--secondary text-subtitle-2"
-							>
-								<v-col
-									lg="6"
-									cols="12"
-									class="py-0"
-								>
-									<v-icon size="15">
-										mdi-tools
-									</v-icon>
-									{{ $t( 'toolsfound', [ numTools ] ) }}
-								</v-col>
-								<v-col
-									lg="6"
-									cols="12"
-									class="py-0"
-								>
-									<v-icon
-										size="16"
-										class="me-1"
-									>
-										mdi-update
-									</v-icon>
-
-									<a href="/crawler-history">
-										{{ $t(
-											'newtoolsfound',
-											[
-												lastCrawlerRun.new_tools +
-													lastCrawlerRun.updated_tools
-											]
-										) }}
-										{{ $t(
-											'tools-lastupdated',
-											[
-												formatDate(
-													lastCrawlerRun.end_date
-												)
-											]
-										) }}
-									</a>
-								</v-col>
-							</v-row>
-						</v-col>
-						<v-col cols="auto">
-							<v-avatar
-								class="ma-3"
-								size="125"
-								tile
-							>
-								<v-img src="/static/img/logo-community.svg" />
-							</v-avatar>
-						</v-col>
-					</v-row>
-				</v-card>
+				<SearchBar
+					@search="onSearchBarSearch"
+				/>
 			</v-col>
 		</v-row>
 
