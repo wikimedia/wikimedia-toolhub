dd4d9e1799fc34c3a32fae27dd47bf692b8885c9
Bryan Davis <bd808@wikimedia.org>
2021-10-10 17:12:24 -0600

    k8s: Add helper script for running crawler
    Add a helper script for use in Kubernetes CronJobs that will attempt to
    signal an Envoy process running on port 1666 to shutdown when the script
    exits. This is intended to help shut the Jobs pod down cleanly by
    allowing the Envoy sidecar to terminate when the main container exits.
    
    Bug: T292861
    Change-Id: I9e3e7f0009cfff0981d320613acf14a143707d7f

---
+++ contrib/cronjob/crawler.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# Copyright (c) 2021 Wikimedia Foundation and contributors.
+# All Rights Reserved.
+#
+# This file is part of Toolhub.
+#
+# Toolhub is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Toolhub is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Toolhub.  If not, see <http://www.gnu.org/licenses/>.
+
+set -Eeuo pipefail
+
+# Send shutdown signal to envoy process running at 127.0.0.1:1666
+# https://envoyproxy.io/docs/envoy/latest/operations/admin#post--quitquitquit
+# Bug: T292861
+kill_envoy() {
+    printf "POST /quitquitquit HTTP/1.0\r\n\r\n" >/dev/tcp/127.0.0.1/1666
+}
+trap 'kill_envoy' EXIT
+
+/usr/local/bin/poetry run python3 /srv/app/manage.py crawl --quiet
