2ea3a4f7c3e717a2fc8c8932b7df372778a63974
Raymond Ndibe <rolisaemeka-ctr@wikimedia.org>
2021-12-22 13:25:00 +0100

    ui: Add autocomplete/lookahead search to home page search bar
    Acceptance Criteria:
    * Given a tool is registered in toolhub, when I start typing the name of
      the tool on the home page search bar, I expect to see a list of tools
      that match my search criteria
    * Given a tool does not exist in toolhub, when I start typing the name
      of the tool on the home page search bar, I expect to see a message
      stating no tool was found
    
    Bug: T293190
    Change-Id: I8ca50d23b09df226ae4603a78f4bef24afd55c57

--- vue/src/components/search/SearchBar.vue
+++ vue/src/components/search/SearchBar.vue
@@ -1,13 +1,17 @@
 <template>
-	<v-text-field
+	<v-combobox
 		v-model="query"
 		class="searchbar"
 		:label="$t( 'search-label' )"
-		:loading="loading"
+		:loading="loading || toolAutoCompleteLoading"
+		:search-input.sync="query"
+		:menu-props="menuProps"
+		:items="getToolAutoCompleteResultsArr()"
 		solo
 		clearable
 		hide-details
-		@keydown.enter="onSearch"
+		@keyup.enter="onSearch"
+		@change="routeToToolsView"
 		@click:clear="onClear"
 	>
 		<template #append>
@@ -26,10 +30,32 @@
 				</v-icon>
 			</v-btn>
 		</template>
-	</v-text-field>
+		<template #item="data">
+			<v-list-item-content>
+				<v-list-item-title>
+					<dl class="row ma-0">
+						<dt class="me-1">{{ data.item }}</dt>
+						<dd>({{ toolAutoCompleteResults[data.item] }})</dd>
+					</dl>
+				</v-list-item-title>
+			</v-list-item-content>
+		</template>
+
+		<template v-if="!toolAutoCompleteLoading" #no-data>
+			<v-list-item>
+				<v-list-item-content>
+					<v-list-item-title>
+						{{ $t( 'tool-not-found', [ query ] ) }}
+					</v-list-item-title>
+				</v-list-item-content>
+			</v-list-item>
+		</template>
+	</v-combobox>
 </template>
 
 <script>
+import { mapState } from 'vuex';
+
 function onSearch() {
 	this.$emit( 'search', this.query );
 }
@@ -43,6 +69,28 @@ function setQuery( query ) {
 	this.query = query;
 }
 
+function performToolAutoComplete( v ) {
+	this.toolAutoCompleteLoading = true;
+	const payload = {
+		query: v
+	};
+	this.$store.dispatch( 'search/autoCompleteTools', payload ).finally(
+		() => {
+			this.toolAutoCompleteLoading = false;
+		}
+	);
+}
+
+function routeToToolsView( name ) {
+	this.$router.push( { name: 'tools-view', params: { name } } );
+}
+
+function getToolAutoCompleteResultsArr() {
+	return this.toolAutoCompleteResults ?
+		Object.keys( this.toolAutoCompleteResults ) :
+		[];
+}
+
 export default {
 	name: 'SearchBar',
 	props: {
@@ -51,12 +99,31 @@ export default {
 		}
 	},
 	data: () => ( {
-		query: null
+		query: '',
+		toolAutoCompleteLoading: false
 	} ),
+	computed: {
+		menuProps() {
+			return !this.query ? { value: false } : {};
+		},
+		...mapState( 'search', {
+			toolAutoCompleteResults: 'toolAutoCompleteResults'
+		} )
+	},
 	methods: {
 		onSearch,
 		onClear,
-		setQuery
+		setQuery,
+		performToolAutoComplete,
+		getToolAutoCompleteResultsArr,
+		routeToToolsView
+	},
+	watch: {
+		query: {
+			handler( val ) {
+				this.performToolAutoComplete( val );
+			}
+		}
 	}
 };
 </script>
